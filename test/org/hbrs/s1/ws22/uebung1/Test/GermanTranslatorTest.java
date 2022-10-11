e27b84819d9ac8e19d49b2d9d79a4a1446b1f0f5
slumglock <leon.gl@web.de>
2022-10-11 20:06:52 +0200

    Tests mit Junit5, 3 Negativ 1 Positivtest

--- test/org/hbrs/s1/ws22/uebung1/Test/GermanTranslatorTest.java
+++ test/org/hbrs/s1/ws22/uebung1/Test/GermanTranslatorTest.java
@@ -2,7 +2,35 @@ package org.hbrs.s1.ws22.uebung1.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.hbrs.s1.ws22.uebung1.*;
+import org.hbrs.s1.ws22.uebung1.control.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class GermanTranslatorTest {
+public class GermanTranslatorTest {
+    private GermanTranslator translator = null;
+
+    @BeforeEach
+    void setup() {
+        translator = (GermanTranslator) Factory.createGermanTranslator();
+    }
+
+    @AfterEach
+    void teardown() {translator = null;}
+
+    @Test
+    void testTranslateNumber() {
+        //Negativtests
+        assertEquals("Übersetzung der Zahl -123 nicht möglich (1.0)", translator.translateNumber(-123), "Fehler bei translateNumber");
+        assertEquals("Übersetzung der Zahl 12000 nicht möglich (1.0)", translator.translateNumber(12000), "Fehler bei translateNumber");
+        assertEquals("Übersetzung der Zahl 0 nicht möglich (1.0)", translator.translateNumber(0), "Fehler bei translateNumber");
+
+        //Positivtests
+        assertEquals("fuenf", translator.translateNumber(5), "Fehler bei translateNumber");
+
+
+    }
+
+
 
 }
